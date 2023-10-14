0dbe06b88c9ab378c06c00fa45c5dcc1b8401166
Athul S <athuls121@gmail.com>
2023-10-14 03:16:52 -0400

    update deployment test - production - v3

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Simple Flask Application'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+    return 'Hello, Simple Flask application'
--- app_bkp.py
+++ app_bkp.py
@@ -4,3 +4,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Simple Flask application'
+
+
+
+
+####################
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Simple Flask Application'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
