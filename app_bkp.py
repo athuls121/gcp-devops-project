845e59f040f77be63377e9627f9dc86c042bd5a3
Athul S <athuls121@gmail.com>
2023-10-14 04:00:23 -0400

    update deployment test - production - v4

--- app_bkp.py
+++ app_bkp.py
@@ -4,22 +4,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Simple Flask application'
-
-
-
-
-####################
-
-
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Simple Flask Application'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
