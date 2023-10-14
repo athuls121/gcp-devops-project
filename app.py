0727c27c56570ceeaad79c1a0268684f85c20666
Athul S <athuls121@gmail.com>
2023-10-14 04:09:18 -0400

    update deployment test - production - v5

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form['name']
+    greeting = f"Hello, {name}! Welcome to the Luxurious Flask Application."
+    return render_template('home.html', greeting=greeting)
 
 if __name__ == '__main__':
     app.run(debug=True)
